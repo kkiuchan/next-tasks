e608eafc3bc99cc16dd71b6df300437ad89944e3
kkiuchan <bandman.gh.bs.dk.lav@gmail.com>
2025-03-10 21:24:09 +0900

    route.tsのGETの引数をPromiseを用いて修正

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   _: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     await connectDb();
